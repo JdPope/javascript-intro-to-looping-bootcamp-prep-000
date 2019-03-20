602ba9e1cdf75fa15983407f8b0b56099cfbd0f9
Jessica Pope <jessica.pope.co@gmail.com>
2019-03-20 21:54:50 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -5,4 +5,6 @@ function forLoop(array) {for (let i = 0; i <25; i++){
       array.push(`I am ${i} strange loops.`)}
           }
 return array}
-function whileLoop(n)
+function whileLoop(n){while(n>0){
+  console.log(--n)
+}return "done"}
